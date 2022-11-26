76b1a1e44dd1ce9a3ed2a361159cac719ef7621d
akendel <andreas.kendel@empulse.de>
2022-11-26 21:17:23 +0100

    Commit project bones
    Add a blank spring boot application that was generated from https://start.spring.io/

---
+++ src/main/java/de/obi/challenge/zombieattacksimulator/ZombieAttackSimulatorApplication.java
@@ -0,0 +1,12 @@
+package de.obi.challenge.zombieattacksimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZombieAttackSimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZombieAttackSimulatorApplication.class, args);
+	}
+}
---
+++ src/test/java/de/obi/challenge/zombieattacksimulator/ZombieAttackSimulatorApplicationTests.java
@@ -0,0 +1,13 @@
+package de.obi.challenge.zombieattacksimulator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZombieAttackSimulatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
